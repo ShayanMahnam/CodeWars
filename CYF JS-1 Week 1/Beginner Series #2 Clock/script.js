f9bbd8814458858f0634e518b1deadee42712972
Shayan Mahnam <95313895+Shayanma94@users.noreply.github.com>
2022-11-23 16:25:34 +0000

    3 kata done

--- CYF JS-1 Week 1/Beginner Series #2 Clock/script.js
+++ CYF JS-1 Week 1/Beginner Series #2 Clock/script.js
@@ -1,6 +1,31 @@
 //My Solution
-
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
 //correct answers
-
+function past(h, m, s) {
+  return (h * 3600 + m * 60 + s) * 1000;
+}
+//
+const past = (h, m, s) => 1000 * (3600 * h + 60 * m + s);
+//
+function past(h, m, s) {
+  var miliseconds = 0;
+  miliseconds = miliseconds + s * 1000;
+  miliseconds = miliseconds + m * 60000;
+  miliseconds = miliseconds + h * 3600000;
+  return miliseconds;
+}
 //Sample Tests
 
+const { assert } = require("chai");
+
+describe("Fixed Tests", () => {
+  it("Tests", () => {
+    assert.strictEqual(past(0, 1, 1), 61000);
+    assert.strictEqual(past(1, 1, 1), 3661000);
+    assert.strictEqual(past(0, 0, 0), 0);
+    assert.strictEqual(past(1, 0, 1), 3601000);
+    assert.strictEqual(past(1, 0, 0), 3600000);
+  });
+});
--- CYF JS-1 Week 1/Opposite number/script.js
+++ CYF JS-1 Week 1/Opposite number/script.js
@@ -1,6 +1,49 @@
 //My Solution
-
+function opposite(number) {
+  let oppNum = 0 - number;
+  return oppNum;
+}
 //correct answers
-
+function opposite(number) {
+  return -number;
+}
+//
+function opposite(number) {
+  return -number;
+}
+//
+const opposite = (number) => -number;
+//
+function opposite(number) {
+  return number * -1;
+}
 //Sample Tests
+const { assert } = require("chai");
 
+describe("Fixed tests", () => {
+  it("Is it a function?", () => {
+    assert.strictEqual(
+      typeof opposite,
+      "function",
+      "opposite should be a function"
+    );
+  });
+  it("should return -1", () => {
+    assert.strictEqual(opposite(1), -1);
+  });
+  it("should return 0", () => {
+    assert.strictEqual(opposite(0), 0);
+  });
+  it("should return -4.25", () => {
+    assert.strictEqual(opposite(4.25), -4.25);
+  });
+  it("should return -3.3333333", () => {
+    assert.strictEqual(opposite(3.3333333), -3.3333333);
+  });
+  it("should return 12525220.3325", () => {
+    assert.strictEqual(opposite(-12525220.3325), 12525220.3325);
+  });
+  it("should return 5", () => {
+    assert.strictEqual(opposite(-5), 5);
+  });
+});
